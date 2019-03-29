4ad8c542f9fb9be4fe17c2876e584748d507b961
berkanbati <scrofleer@gmail.com>
2019-03-29 05:49:23 +0300

    Commit

---
+++ Subnet Scanner/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Subnet_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.maskedTextBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.stop);
+            this.panel1.Controls.Add(this.start);
+            this.panel1.Location = new System.Drawing.Point(155, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 123);
+            this.panel1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(62, 11);
+            this.maskedTextBox1.Mask = "000/000/0";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(83, 24);
+            this.maskedTextBox1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.lblStatus);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(24, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(292, 23);
+            this.panel2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 6;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(43, 4);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ex: 192.168.1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 94);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(322, 13);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Subnet";
+            // 
+            // stop
+            // 
+            this.stop.Enabled = false;
+            this.stop.Location = new System.Drawing.Point(228, 13);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(53, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(169, 13);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(53, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader1,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(340, 198);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(334, 212);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "IP";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Host Info";
+            this.columnHeader1.Width = 170;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            this.columnHeader3.Width = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "All Machines";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(337, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "In The Network";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 413);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 9;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView2.Location = new System.Drawing.Point(12, 198);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(244, 212);
+            this.listView2.TabIndex = 10;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "IP";
+            this.columnHeader4.Width = 80;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Host Info";
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Status";
+            this.columnHeader6.Width = 80;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 453);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+
+    }
+}
+
---
+++ Subnet Scanner/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net;
+using System.Management;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Subnet_Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label6.ForeColor = Color.Red;
+            label6.Text = "NULL";
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Thread myThread = null;
+
+        public void scanner(string subnet)
+        {
+            Ping myPing;
+            PingReply reply;
+            IPAddress addr;
+            IPHostEntry host;
+
+            progressBar1.Maximum = 254;
+            progressBar1.Value = 0;
+            listView1.Items.Clear();
+            int count2 = 0;
+
+            for (int i = 0; i < 255; i++)
+            {
+                string subnetv2 = "." + i.ToString();
+                myPing = new Ping();
+                reply = myPing.Send(subnet + subnetv2, 900);
+                label6.ForeColor = Color.Green;
+                label6.Text = "Scan.." + subnet + subnetv2;
+
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    try
+                    {
+                        addr = IPAddress.Parse(subnet + subnetv2);
+                        host = Dns.GetHostEntry(addr);
+                        listView1.Items.Add(new ListViewItem(new String[] { subnet + subnetv2, host.HostName, "Ready" }));
+                        listView2.Items.Add(new ListViewItem(new String[] { subnet + subnetv2, host.HostName, "Online" }));
+
+                    }
+                    catch
+                    {
+                        if (i > 1)
+                        {
+                            listView2.Items.Add(new ListViewItem(new String[] { subnet + subnetv2, "Unknown", "Online" }));
+                        }
+                    }
+                    if (i > 1)
+                    {
+                        label7.Text = listView2.Items.Count.ToString();
+                    }
+
+                }
+                progressBar1.Value += 1;
+            }
+
+            start.Enabled = true;
+            maskedTextBox1.Enabled = false;
+            label6.Text = "FINISH";
+            int count = listView1.Items.Count;
+            MessageBox.Show("Scan Finished!\n " + label7.Text.ToString() + " Host Find.", "Finish", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            maskedTextBox1.Text = "";
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+            label7.Text = "";
+            listView1.Items.Clear();
+
+            if (maskedTextBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("You didn't give Ip Adress", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                myThread = new Thread(() => scanner(maskedTextBox1.Text));
+                myThread.Start();
+
+                if (myThread.IsAlive == true)
+                {
+                    stop.Enabled = true;
+                    start.Enabled = false;
+                    maskedTextBox1.Enabled = false;
+                }
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            myThread.Suspend();
+            start.Enabled = true;
+            stop.Enabled = false;
+            maskedTextBox1.Enabled = true;
+            label6.ForeColor = System.Drawing.Color.Red;
+            label6.Text = "NULL";
+            maskedTextBox1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
